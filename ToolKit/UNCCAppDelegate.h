9ffd87822b09260d3a8e73e754e6559e4d232ea6
Matt Senn <msenn2@ccid169.uncc.edu>
2012-02-21 14:23:44 -0500

    Initial Commit

---
+++ ToolKit/UNCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UNCCAppDelegate.h
+//  ToolKit
+//
+//  Created by Matt Senn on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UNCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToolKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToolKit
+//
+//  Created by Matt Senn on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UNCCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UNCCAppDelegate class]));
+    }
+}
