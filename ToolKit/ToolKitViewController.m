84d3f998bd2abc7d0a8916489784b9eb3a9f62ba
eternalmatt@gmail.com <msenn2@ccid169.uncc.edu>
2012-03-13 11:35:50 -0400

    View Controller now uses a UIScrollView so the user can scroll through the views easily.

--- ToolKit/ToolKitViewController.m
+++ ToolKit/ToolKitViewController.m
@@ -12,7 +12,6 @@
 #import "CameraView.h"
 
 @interface ToolKitViewController ()
-
 @end
 
 @implementation ToolKitViewController
@@ -22,18 +21,30 @@
     [super loadView];
     
     CGRect bounds = [[UIScreen mainScreen] bounds];
-    bounds = CGRectMake(bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height/2.5);
-    [self.view addSubview:[[CameraView alloc] initWithFrame:bounds]];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:bounds];
     
     
-    bounds = CGRectMake(bounds.origin.x, bounds.size.height, bounds.size.width, [UIScreen mainScreen].bounds.size.height - bounds.size.height);
-    [self.view addSubview:[[GPSView alloc] initWithFrame:bounds]];
+    bounds = CGRectMake(bounds.origin.x, bounds.origin.y, bounds.size.width, 192);
+    [scrollView addSubview:[[CameraView alloc] initWithFrame:bounds]];
     
+    
+    bounds = CGRectMake(bounds.origin.x, bounds.size.height, bounds.size.width, 250);
+    [scrollView addSubview:[[GPSView alloc] initWithFrame:bounds]];
+    
+    CGSize contentSize = CGSizeMake(scrollView.frame.size.width, 0);
+    for (UIView *view in scrollView.subviews)
+    {
+        contentSize.height += view.frame.size.height;
+    }
+    
+    scrollView.contentSize = contentSize;
+    scrollView.pagingEnabled = YES;
+    [self.view addSubview:scrollView];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return interfaceOrientation == UIInterfaceOrientationPortrait;
 }
 
 @end
