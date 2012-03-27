fe4c1e196169fa37910665e175e105380cf8a7d3
Matt Senn <eternalmatt@gmail.com>
2012-03-27 15:05:55 -0400

    Initial commit of AudioView

---
+++ ToolKit/AudioView.h
@@ -0,0 +1,13 @@
+//
+//  AudioView.h
+//  ToolKit
+//
+//  Created by Matt Senn on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AudioView : UIView
+
+@end
---
+++ ToolKit/AudioView.m
@@ -0,0 +1,31 @@
+//
+//  AudioView.m
+//  ToolKit
+//
+//  Created by Matt Senn on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioView.h"
+
+@implementation AudioView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
